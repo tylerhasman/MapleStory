66da07a9d71b9532893bda900a299a8718748a93
Tyler <tylerhasman10@hotmail.com>
2016-11-18 20:08:14 -0800

    optimized to byte from string convertor method

--- src/maplestory/util/Hex.java
+++ src/maplestory/util/Hex.java
@@ -1,13 +1,7 @@
 package maplestory.util;
 
-import java.io.ByteArrayOutputStream;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Hex {
 
-	private static Map<String, Object> cache = new HashMap<>();
-	
 	public static String toHex(byte[] bytes){
 		StringBuilder sb = new StringBuilder();
 	    for (byte b : bytes) {
@@ -18,48 +12,23 @@ public class Hex {
 	}
 
 	public static byte[] toByteArray(String hex) {
-		if(cache.containsKey(hex)){
-			return (byte[]) cache.get(hex);
-		}
+	
 		byte[] b = getByteArrayFromHexString(hex);
-		cache.put(hex, b);
+		
 		return b;
 	}
 	
+	/**
+	 * Credit to http://stackoverflow.com/a/140861/2284885
+	 */
 	private static byte[] getByteArrayFromHexString(String hex) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        int nexti = 0;
-        int nextb = 0;
-        boolean highoc = true;
-        outer:
-        for (;;) {
-            int number = -1;
-            while (number == -1) {
-                if (nexti == hex.length()) {
-                    break outer;
-                }
-                char chr = hex.charAt(nexti);
-                if (chr >= '0' && chr <= '9') {
-                    number = chr - '0';
-                } else if (chr >= 'a' && chr <= 'f') {
-                    number = chr - 'a' + 10;
-                } else if (chr >= 'A' && chr <= 'F') {
-                    number = chr - 'A' + 10;
-                } else {
-                    number = -1;
-                }
-                nexti++;
-            }
-            if (highoc) {
-                nextb = number << 4;
-                highoc = false;
-            } else {
-                nextb |= number;
-                highoc = true;
-                baos.write(nextb);
-            }
-        }
-        return baos.toByteArray();
+	    int len = hex.length();
+	    byte[] data = new byte[len / 2];
+	    for (int i = 0; i < len; i += 2) {
+	        data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+	                             + Character.digit(hex.charAt(i+1), 16));
+	    }
+	    return data;
     }
 	
 }

c7dadaf8df146ca866a4aa0d8d1264bde40ffd08
Tyler Hasman <tylerhasman10@hotmail.com>
2017-10-13 22:46:17 +0900

    made inventory thread safe

--- src/maplestory/inventory/MapleInventory.java
+++ src/maplestory/inventory/MapleInventory.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -28,68 +29,94 @@ public class MapleInventory implements Inventory {
 	private InventoryType type;
 	private WeakReference<MapleCharacter> owner;
 	
+	private ReentrantLock operationLock;
+	
 	public MapleInventory(MapleCharacter owner, int maxSize, InventoryType type) {
 		items = new HashMap<>(maxSize);
 		this.maxSize = maxSize;
 		this.type = type;
 		this.owner = new WeakReference<MapleCharacter>(owner);
+		operationLock = new ReentrantLock(true);
 	}
 	
 	@Override
 	public final Map<Integer, Item> getItems() {
-		return Collections.unmodifiableMap(items);
+		operationLock.lock();
+		try{
+			return Collections.unmodifiableMap(items);
+		}finally{
+			operationLock.unlock();
+		}
 	}
 	
 	@Override
 	public boolean hasSpace(int amountOfItems) {
-		return items.size() + amountOfItems <= maxSize;
+		operationLock.lock();
+		try{
+			return items.size() + amountOfItems <= maxSize;
+		}finally{
+			operationLock.unlock();
+		}
 	}
 	
 	@Override
 	public void transferToCashInventory(int slot, CashShopInventory cashInventory) {
+		operationLock.lock();
 		
-		Item item = getItem(slot);
+		try{
+			Item item = getItem(slot);
 
-		System.out.println(slot);
-		if(item == null){
-			return;
-		}
-		
-		
-		if(item instanceof CashItem){
-			setItemInternal(slot, null);
-			cashInventory.addItem(item);
-
-			if(getOwner() != null){
-				getOwner().getClient().sendPacket(PacketFactory.cashShopAddItem((CashItem) item, getOwner().getClient().getId()));
+			if(item == null){
+				return;
 			}
 			
-		}else{
-			throw new IllegalArgumentException("Item at slot "+slot+" is not a cash item. It is a "+item.getClass().getName()+" toString: "+item.toString());
+			if(item instanceof CashItem){
+				setItemInternal(slot, null);
+				cashInventory.addItem(item);
+
+				if(getOwner() != null){
+					getOwner().getClient().sendPacket(PacketFactory.cashShopAddItem((CashItem) item, getOwner().getClient().getId()));
+				}
+				
+			}else{
+				throw new IllegalArgumentException("Item at slot "+slot+" is not a cash item. It is a "+item.getClass().getName()+" toString: "+item.toString());
+			}	
+		}finally{
+			operationLock.unlock();
 		}
 		
 	}
 	
 	@Override
 	public Item firstOf(int itemId) {
-		for(Item item : items.values()){
-			if(item.getItemId() == itemId){
-				return item;
+		operationLock.lock();
+		try{
+			for(Item item : items.values()){
+				if(item.getItemId() == itemId){
+					return item;
+				}
 			}
+			return null;
+		}finally{
+			operationLock.unlock();
 		}
-		return null;
 	}
 	
 	@Override
 	public int findByCashId(int cashUniqueId) {
-		for(Entry<Integer, Item> item : items.entrySet()){
-			if(item.getValue() instanceof CashItem){
-				if(((CashItem) item.getValue()).getUniqueId() == cashUniqueId){
-					return item.getKey();
+		operationLock.lock();
+		try{
+			for(Entry<Integer, Item> item : items.entrySet()){
+				if(item.getValue() instanceof CashItem){
+					if(((CashItem) item.getValue()).getUniqueId() == cashUniqueId){
+						return item.getKey();
+					}
 				}
 			}
+			return -1;
+		}finally{
+			operationLock.unlock();
 		}
-		return -1;
 	}
 	
 	@Override
@@ -99,32 +126,40 @@ public class MapleInventory implements Inventory {
 	
 	@Override
 	public boolean hasSpace(int itemId, int quantity) {
-		
-		if(hasSpace(Math.max(quantity, ItemInfoProvider.getSlotMax(itemId)) / ItemInfoProvider.getSlotMax(itemId))){
-			return true;
-		}
-		
-		for(Item item : items.values()){
-			if(item.getItemId() == itemId){
-				int amountCanAdd = ItemInfoProvider.getSlotMax(itemId) - item.getAmount();
-				quantity -= amountCanAdd;
+		operationLock.lock();
+		try{
+			if(hasSpace(Math.max(quantity, ItemInfoProvider.getSlotMax(itemId)) / ItemInfoProvider.getSlotMax(itemId))){
+				return true;
 			}
+			
+			for(Item item : items.values()){
+				if(item.getItemId() == itemId){
+					int amountCanAdd = ItemInfoProvider.getSlotMax(itemId) - item.getAmount();
+					quantity -= amountCanAdd;
+				}
+			}
+			
+			return quantity <= 0;
+		}finally{
+			operationLock.unlock();
 		}
-		
-		return quantity <= 0;
 	}
 	
 	@Override
 	public int countById(int itemId) {
-		int count = 0;
-		for(Item item : listById(itemId)){
-			count += item.getAmount();
+		operationLock.lock();
+		try{
+			int count = 0;
+			for(Item item : listById(itemId)){
+				count += item.getAmount();
+			}
+			return count;
+		}finally{
+			operationLock.unlock();
 		}
-		return count;
 	}
 	
-	@Override
-	public void moveItem(int slot, int destination) {
+	private void moveItemUnsafe(int slot, int destination){
 		if(slot < 0 || destination < 0){
 			return;
 		}
@@ -168,17 +203,31 @@ public class MapleInventory implements Inventory {
 		sendPacket(PacketFactory.getInventoryOperationPacket(true, ops));
 	}
 	
-	private void swap(int one, int two){
-		Item itemOne = getItem(one);
-		Item itemTwo = getItem(two);
-		
-		setItemInternal(two, itemOne);
-		setItemInternal(one, itemTwo);
+	@Override
+	public void moveItem(int slot, int destination) {
+		operationLock.lock();
+		try{
+			moveItemUnsafe(slot, destination);
+		}finally{
+			operationLock.unlock();
+		}
 	}
 	
-	@Override
-	public void dropItem(int slot, int amount) {
-		Item dropped = getItem(slot);
+	private void swap(int one, int two){
+		operationLock.lock();
+		try{
+			Item itemOne = getItem(one);
+			Item itemTwo = getItem(two);
+			
+			setItemInternal(two, itemOne);
+			setItemInternal(one, itemTwo);
+		}finally{
+			operationLock.unlock();
+		}
+	}
+	
+	private void dropItemUnsafe(int slot, int amount){
+Item dropped = getItem(slot);
 		
 		if(dropped == null || amount <= 0){
 			return;
@@ -219,108 +268,131 @@ public class MapleInventory implements Inventory {
 				getOwner().getMap().dropItem(dropped, getOwner().getPosition(), getOwner());
 			}
 		}
-		
+	}
+	
+	@Override
+	public void dropItem(int slot, int amount) {
+		operationLock.lock();
+		try{
+			dropItemUnsafe(slot, amount);
+		}finally{
+			operationLock.unlock();
+		}
 	}
 	
 	@Override
 	public void removeItemFromSlot(int slot, int amount) {
-		Item item = getItem(slot);
-		
-		item.setAmount(item.getAmount() - amount);
-		
-		if(item.getAmount() <= 0){
-			setItemInternal(slot, null);
-			sendPacket(PacketFactory.getInventoryOperationPacket(true, Collections.singletonList(InventoryOperation.removeItem(item, slot))));
-		}else{
-			sendPacket(PacketFactory.getInventoryOperationPacket(true, Collections.singletonList(InventoryOperation.updateAmount(item, slot))));
+		operationLock.lock();
+		try{
+			Item item = getItem(slot);
+			
+			item.setAmount(item.getAmount() - amount);
+			
+			if(item.getAmount() <= 0){
+				setItemInternal(slot, null);
+				sendPacket(PacketFactory.getInventoryOperationPacket(true, Collections.singletonList(InventoryOperation.removeItem(item, slot))));
+			}else{
+				sendPacket(PacketFactory.getInventoryOperationPacket(true, Collections.singletonList(InventoryOperation.updateAmount(item, slot))));
+			}
+		}finally{
+			operationLock.unlock();
 		}
 	}
 	
 	@Override
 	public int getProjectileId(int bulletCount, Item weapon) {
-		
-		MapleWeaponType weaponType = ItemInfoProvider.getWeaponType(weapon.getItemId());
-		
-		MapleCharacter chr = (MapleCharacter) getOwner();
-		
-		Set<Integer> slotSet = items.keySet();
-		List<Integer> slots = slotSet.stream().sorted().collect(Collectors.toList());
-		
-		for(int slot : slots){
-			Item item = getItem(slot);
+		operationLock.lock();
+		try{
+			MapleWeaponType weaponType = ItemInfoProvider.getWeaponType(weapon.getItemId());
 			
-			int id = item.getItemId();
+			MapleCharacter chr = (MapleCharacter) getOwner();
 			
-			boolean bow = item.isA(ItemType.ARROW);
-			boolean cbow = item.isA(ItemType.BOLT);
-			boolean star = item.isA(ItemType.THROWING_STAR);
-			boolean bullet = item.isA(ItemType.BULLET);
-			boolean isMagicalMitten = weapon.getItemId() == 1472063;
+			Set<Integer> slotSet = items.keySet();
+			List<Integer> slots = slotSet.stream().sorted().collect(Collectors.toList());
 			
-			// 1472063 is Magical Mitten. It allows players to throw arrows :O
-			
-			if(item.getAmount() >= bulletCount){
-				if(weaponType == MapleWeaponType.CLAW && star && !isMagicalMitten){
-					if (!((id == 2070007 || id == 2070018) && chr
-							.getLevel() < 70)
-							|| (id == 2070016 && chr.getLevel() < 50)) {
-						return id;
-					}
-				}else if(weaponType == MapleWeaponType.GUN && bullet){
-					if (id == 2331000 && id == 2332000) {
-						if (chr.getLevel() > 69) {
+			for(int slot : slots){
+				Item item = getItem(slot);
+				
+				int id = item.getItemId();
+				
+				boolean bow = item.isA(ItemType.ARROW);
+				boolean cbow = item.isA(ItemType.BOLT);
+				boolean star = item.isA(ItemType.THROWING_STAR);
+				boolean bullet = item.isA(ItemType.BULLET);
+				boolean isMagicalMitten = weapon.getItemId() == 1472063;
+				
+				// 1472063 is Magical Mitten. It allows players to throw arrows :O
+				
+				if(item.getAmount() >= bulletCount){
+					if(weaponType == MapleWeaponType.CLAW && star && !isMagicalMitten){
+						if (!((id == 2070007 || id == 2070018) && chr
+								.getLevel() < 70)
+								|| (id == 2070016 && chr.getLevel() < 50)) {
 							return id;
 						}
-					} else if (chr.getLevel() > (id % 10) * 20 + 9) {
+					}else if(weaponType == MapleWeaponType.GUN && bullet){
+						if (id == 2331000 && id == 2332000) {
+							if (chr.getLevel() > 69) {
+								return id;
+							}
+						} else if (chr.getLevel() > (id % 10) * 20 + 9) {
+							return id;
+						}
+					}else if((weaponType == MapleWeaponType.BOW && bow) 
+							|| (weaponType == MapleWeaponType.CROSSBOW && cbow)
+							|| (weapon.getItemId() == 1472063 && (bow || cbow))){
 						return id;
 					}
-				}else if((weaponType == MapleWeaponType.BOW && bow) 
-						|| (weaponType == MapleWeaponType.CROSSBOW && cbow)
-						|| (weapon.getItemId() == 1472063 && (bow || cbow))){
-					return id;
 				}
 			}
+			
+			return 0;
+		}finally{
+			operationLock.unlock();
 		}
-		
-		return 0;
 	}
 	
 	@Override
 	public boolean removeItem(int id, int amount) {
-		if(amount == 0){
-			return true;
-		}
-		List<Pair<Integer, Item>> existing = listByIdWithSlot(id);
-		List<InventoryOperation> actions = new ArrayList<>();
-		
-		for(Pair<Integer, Item> pair : existing){
-			if(amount <= 0){
-				break;
+		operationLock.lock();
+		try{
+			if(amount == 0){
+				return true;
 			}
-			int slot = pair.getLeft();
-			Item item = pair.getRight();
+			List<Pair<Integer, Item>> existing = listByIdWithSlot(id);
+			List<InventoryOperation> actions = new ArrayList<>();
 			
-			if(item.getAmount() > amount){
-				int oldAmount = item.getAmount();
-				item.setAmount(item.getAmount() - amount);
-				amount -= oldAmount;
-				actions.add(InventoryOperation.updateAmount(item, slot));
-			}else{// only possibility left is (item.getAmount() < amount)
-				amount -= item.getAmount();
-				if(!ItemType.RECHARGABLE.isThis(id)){
-					setItemInternal(slot, null);
-					actions.add(InventoryOperation.removeItem(item, slot));
-				}else{
-					item.setAmount(0);
-					actions.add(InventoryOperation.updateAmount(item, slot));
+			for(Pair<Integer, Item> pair : existing){
+				if(amount <= 0){
+					break;
 				}
+				int slot = pair.getLeft();
+				Item item = pair.getRight();
 				
+				if(item.getAmount() > amount){
+					int oldAmount = item.getAmount();
+					item.setAmount(item.getAmount() - amount);
+					amount -= oldAmount;
+					actions.add(InventoryOperation.updateAmount(item, slot));
+				}else{// only possibility left is (item.getAmount() < amount)
+					amount -= item.getAmount();
+					if(!ItemType.RECHARGABLE.isThis(id)){
+						setItemInternal(slot, null);
+						actions.add(InventoryOperation.removeItem(item, slot));
+					}else{
+						item.setAmount(0);
+						actions.add(InventoryOperation.updateAmount(item, slot));
+					}
+					
+				}
 			}
-		}
 
-		sendPacket(PacketFactory.getInventoryOperationPacket(true, actions));
-		
-		return amount <= 0;
+			sendPacket(PacketFactory.getInventoryOperationPacket(true, actions));
+			
+			return amount <= 0;
+		}finally{
+			operationLock.unlock();
+		}
 	}
 	
 	@Override
@@ -334,12 +406,22 @@ public class MapleInventory implements Inventory {
 	}
 	
 	public final Item getItem(int slot){
-		return items.get(slot);
+		operationLock.lock();
+		try{
+			return items.get(slot);
+		}finally{
+			operationLock.unlock();
+		}
 	}
 	
 	@Override
 	public boolean isFull() {
-		return items.size() >= getSize();
+		operationLock.lock();
+		try{
+			return items.size() >= getSize();
+		}finally{
+			operationLock.unlock();
+		}
 	}
 	
 	@Override
@@ -348,86 +430,95 @@ public class MapleInventory implements Inventory {
 	}
 	
 	private List<Pair<Integer, Item>> listByIdWithSlot(int itemId){
-		List<Pair<Integer, Item>> filtered = new ArrayList<>();
-		
-		for(int slot : items.keySet()){
-			Item item = items.get(slot);
+		operationLock.lock();
+		try{
+			List<Pair<Integer, Item>> filtered = new ArrayList<>();
 			
-			if(item.getItemId() == itemId){
-				filtered.add(new Pair<Integer, Item>(slot, item));
+			for(int slot : items.keySet()){
+				Item item = items.get(slot);
+				
+				if(item.getItemId() == itemId){
+					filtered.add(new Pair<Integer, Item>(slot, item));
+				}
 			}
+			
+			return filtered;
+		}finally{
+			operationLock.unlock();
 		}
-		
-		return filtered;
 	}
 	
 	@Override
 	public boolean addItem(Item item) {
-		
-		int slotMax = getOwner().getMaxSlotForItem(item);
-		
-		List<Pair<Integer, Item>> existing = listByIdWithSlot(item.getItemId());
-		
-		List<InventoryOperation> changes = new ArrayList<InventoryOperation>();
-		
-		if(item instanceof EquipItem){
-			int slot = getFreeSlot();
-			if(slot > 0){
-				setItemInternal(slot, item);
-				changes.add(InventoryOperation.addItem(item, slot));
-			}
-			sendPacket(PacketFactory.getInventoryOperationPacket(true, changes));
-			return slot > 0;
-		}
-		if(!item.isA(ItemType.RECHARGABLE)){
-			for(Pair<Integer, Item> pair : existing){
-				Item exists = pair.getRight();
-				if(exists.canMerge(item)){
-					int oldAmount = exists.getAmount();
-					if(exists.getAmount() < slotMax){
-						int newAmount = Math.min(exists.getAmount() + item.getAmount(), slotMax);
-						
-						item.setAmount(item.getAmount() - (newAmount - oldAmount));
-						exists.setAmount(newAmount);
-						
-						changes.add(InventoryOperation.updateAmount(exists, pair.getLeft()));
-					}	
-				}
-			}
-			int total = item.getAmount();
-			while(total > 0){
-				int newQ = Math.min(item.getAmount(), slotMax);
-				if(newQ != 0){
-					total -= item.getAmount();
-					Item nItem = item.copyOf(newQ);
-					int slot = getFreeSlot();
-					
-					if(slot == -1){
-						sendPacket(PacketFactory.getInventoryOperationPacket(true, changes));
-						return true;
-					}
-					changes.add(InventoryOperation.addItem(nItem, slot));
-					setItemInternal(slot, nItem);
-					
-				}
-			}
-			sendPacket(PacketFactory.getInventoryOperationPacket(true, changes));
-			return true;
-		}else{
-			int slot = getFreeSlot();
+		operationLock.lock();
+		try{
+			int slotMax = getOwner().getMaxSlotForItem(item);
 			
-			if(slot == -1){
+			List<Pair<Integer, Item>> existing = listByIdWithSlot(item.getItemId());
+			
+			List<InventoryOperation> changes = new ArrayList<InventoryOperation>();
+			
+			if(item instanceof EquipItem){
+				int slot = getFreeSlot();
+				if(slot > 0){
+					setItemInternal(slot, item);
+					changes.add(InventoryOperation.addItem(item, slot));
+				}
+				sendPacket(PacketFactory.getInventoryOperationPacket(true, changes));
+				return slot > 0;
+			}
+			if(!item.isA(ItemType.RECHARGABLE)){
+				for(Pair<Integer, Item> pair : existing){
+					Item exists = pair.getRight();
+					if(exists.canMerge(item)){
+						int oldAmount = exists.getAmount();
+						if(exists.getAmount() < slotMax){
+							int newAmount = Math.min(exists.getAmount() + item.getAmount(), slotMax);
+							
+							item.setAmount(item.getAmount() - (newAmount - oldAmount));
+							exists.setAmount(newAmount);
+							
+							changes.add(InventoryOperation.updateAmount(exists, pair.getLeft()));
+						}	
+					}
+				}
+				int total = item.getAmount();
+				while(total > 0){
+					int newQ = Math.min(item.getAmount(), slotMax);
+					if(newQ != 0){
+						total -= item.getAmount();
+						Item nItem = item.copyOf(newQ);
+						int slot = getFreeSlot();
+						
+						if(slot == -1){
+							sendPacket(PacketFactory.getInventoryOperationPacket(true, changes));
+							return true;
+						}
+						changes.add(InventoryOperation.addItem(nItem, slot));
+						setItemInternal(slot, nItem);
+						
+					}
+				}
+				sendPacket(PacketFactory.getInventoryOperationPacket(true, changes));
+				return true;
+			}else{
+				int slot = getFreeSlot();
+				
+				if(slot == -1){
+					sendPacket(PacketFactory.getInventoryOperationPacket(true, changes));
+					return false;
+				}
+				
+				changes.add(InventoryOperation.addItem(item, slot));
+				
+				setItemInternal(slot, item);
 				sendPacket(PacketFactory.getInventoryOperationPacket(true, changes));
 				return false;
 			}
 			
-			changes.add(InventoryOperation.addItem(item, slot));
-			
-			setItemInternal(slot, item);
-			sendPacket(PacketFactory.getInventoryOperationPacket(true, changes));
-			return false;
+		}finally{
+			operationLock.unlock();
 		}
-		
 	}
 	
 	protected void sendPacket(byte[] packet){
@@ -454,22 +545,32 @@ public class MapleInventory implements Inventory {
 	
 	@Override
 	public List<Item> listById(int itemId) {
-		List<Item> items = new ArrayList<>();
-		
-		for(Item item : this.items.values()){
-			if(item.getItemId() == itemId){
-				items.add(item);
+		operationLock.lock();
+		try{
+			List<Item> items = new ArrayList<>();
+			
+			for(Item item : this.items.values()){
+				if(item.getItemId() == itemId){
+					items.add(item);
+				}
 			}
+			return items;
+		}finally{
+			operationLock.unlock();
 		}
-		return items;
 	}
 	
 	protected void setItemInternal(int slot, Item item){
-		if(item == null){
-			items.remove(slot);
-		}
-		else{
-			items.put(slot, item);
+		operationLock.lock();
+		try{
+			if(item == null){
+				items.remove(slot);
+			}
+			else{
+				items.put(slot, item);
+			}
+		}finally{
+			operationLock.unlock();
 		}
 	}
 	

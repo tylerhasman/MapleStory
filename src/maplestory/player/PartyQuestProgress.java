559328f05eebc867e57783cb302728abc01f2b7f
Tyler Hasman <tylerhasman10@hotmail.com>
2018-02-25 16:00:50 +0900

    Added PartyQuest item tracking

--- src/maplestory/player/PartyQuestProgress.java
+++ src/maplestory/player/PartyQuestProgress.java
@@ -1,6 +1,8 @@
 package maplestory.player;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import database.MapleDatabase;
@@ -15,14 +17,55 @@ public class PartyQuestProgress {
 	@Getter @Setter
 	private boolean riceHatGiven;
 	
+	private List<String> obtainedPartyQuestItems;
+	
 	PartyQuestProgress() {
 		riceCakesGiven = 0;
 		riceHatGiven = false;
+		obtainedPartyQuestItems = new ArrayList<>();
+	}
+	
+	public void addPartyQuestItem(String id) {
+		if(hasPartyQuestItem(id)) {
+			return;
+		}
+		obtainedPartyQuestItems.add(id);
+	}
+	
+	public boolean hasPartyQuestItem(String id) {
+		return obtainedPartyQuestItems.contains(id);
+	}
+	
+	public void removePartyQuestItem(String id) {
+		obtainedPartyQuestItems.remove(id);
+	}
+	
+	private String serializeObtained() {
+		
+		String str = "";
+		
+		for(String s : obtainedPartyQuestItems) {
+			str += s+",";
+		}
+		
+		return str;
+	}
+	
+	private static List<String> deserializeObtained(String serialized){
+		
+		return Arrays.asList(serialized.split(","));
+		
 	}
 	
 	public void saveToDatabase(int characterId) throws SQLException{
 		
-		MapleDatabase.getInstance().execute("UPDATE `party_quest` SET `rice_cakes_given`=?,`rice_hat_given`=? WHERE `player`=?", riceCakesGiven, riceHatGiven, characterId);
+		if(MapleDatabase.getInstance().query("SELECT * FROM `party_quest` WHERE `player`=?", characterId).size() == 0) {
+			
+			MapleDatabase.getInstance().execute("INSERT INTO `party_quest` (`player`, `items`) VALUES (?, ?)", characterId, "");
+			
+		}
+		
+		MapleDatabase.getInstance().execute("UPDATE `party_quest` SET `rice_cakes_given`=?,`rice_hat_given`=?, `items`=? WHERE `player`=?", riceCakesGiven, riceHatGiven, serializeObtained(), characterId);
 		
 	}
 	
@@ -31,7 +74,7 @@ public class PartyQuestProgress {
 		PartyQuestProgress progress = new PartyQuestProgress();
 		
 		try{
-			List<QueryResult> results = MapleDatabase.getInstance().query("SELECT `rice_cakes_given`,`rice_hat_given` FROM `party_quest` WHERE `player`=?", characterId);
+			List<QueryResult> results = MapleDatabase.getInstance().query("SELECT `rice_cakes_given`,`rice_hat_given`,`items` FROM `party_quest` WHERE `player`=?", characterId);
 			
 			if(results.size() > 0){
 				
@@ -39,6 +82,7 @@ public class PartyQuestProgress {
 				
 				progress.riceCakesGiven = result.get("rice_cakes_given");
 				progress.riceHatGiven = (int) result.get("rice_hat_given") == 1;
+				progress.obtainedPartyQuestItems = deserializeObtained(result.get("items"));
 				
 			}
 			
